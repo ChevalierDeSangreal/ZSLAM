8d15f27dfb0fe8fb9b922bcc08eb6748f00ee40c
ChevalierDeSangreal <415545488@qq.com>
2025-05-02 15:51:44 +0800

    Update .gitignore to include *.out

--- cfg/envmove_cfg.py
+++ cfg/envmove_cfg.py
@@ -22,7 +22,7 @@ class AgentCfg:
 class MapCfg:
     width = 5. # 地图宽度
     height = 5. # 地图高度
-    ratio = 0.1 # 地图分辨率
+    ratio = 0.05 # 地图分辨率
     max_coverage = 0.1 # 地图最大覆盖率（0到1之间），以地图总面积的比例计算。
 
 
